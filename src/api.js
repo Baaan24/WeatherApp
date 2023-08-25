035f880e828d60a8adeeaf338c14364df5d1797f
Mirbaan Balagamwala <mbalagamwala3@gatech.edu>
2023-08-25 15:59:52 -0400

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        "X-RapidAPI-Key": "da96b19224mshf710ae844c2db65p109a1fjsn7e6ef8c58a91",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";    
+
+export const WEATHER_API_URL= "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "6923daa9e3162f930cf67c4521c84f8f";
